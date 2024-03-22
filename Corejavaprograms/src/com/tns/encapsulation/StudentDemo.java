87eb3e75e9a52d7ede3136cbef7bba4bcc57e341
91944 <91944@192.168.0.6>
2024-03-22 23:59:56 +0530

    First Commit

---
+++ Corejavaprograms/src/com/tns/encapsulation/StudentDemo.java
@@ -0,0 +1,14 @@
+package com.tns.encapsulation;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student ob=new Student();
+		ob.setName("Hussain");
+		ob.setRollno(22);
+		System.out.println("the values are"+ob);
+		
+	}
+
+}
